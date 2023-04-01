5fdefa95e4c8548481255014a2d132aab9e941db
Aniket Chaurasia <ac226018me@gmail.com>
2023-04-01 10:21:25 +0530

    Implementing exercise 2 using classes

--- Week-4/exercise-2.js
+++ Week-4/exercise-2.js
@@ -1,24 +1,55 @@
-var Person = function() {};
+//! Implementing using prototype
 
-Person.prototype.initialize = function(name,age) { 
-    this.name=name;
-    this.age=age;
-    } // TODO: create the class Teacher and a method teach
+var Person = function () { };
 
+Person.prototype.initialize = function (name, age) {
+    this.name = name;
+    this.age = age;
+}
 
-function Teacher(name){
-    Person.call(this,name);
+ 
+function Teacher(name) {
+    Person.call(this, name);
     this.name = name;
     // this.sub = sub;
 }
 
 Object.setPrototypeOf(Teacher.prototype, Person.prototype)
 
-Teacher.prototype.teach = function(sub){
+Teacher.prototype.teach = function (sub) {
     console.log(`${this.name} is teaching ${sub}`);
 }
 
 
+
 var him = new Teacher();
-him.initialize("Adam",45);
+him.initialize("Adam", 45);
+him.teach("Inheritance");
+
+/*----------------------------------------------------------------------------------------------*/
+
+
+//! Implementing using ES6 Classes
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+
+class Teacher extends Person {
+    constructor(name, sub) {
+        super(name);
+        this.sub = sub;
+    }
+    teach(sub) {
+        console.log(`${this.name} is teaching ${this.sub}.`)
+    }
+}
+
+
+var him = new Teacher("Adam", 45);
+// him.initialize("Adam",45);
 him.teach("Inheritance");
