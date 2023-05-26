d60a11dc8d1d6e22a39eaf56768552559610db12
Aniket Chaurasia <ac226018me@gmail.com>
2023-05-26 10:50:44 +0530

    Mock Interview Files Push

--- Week-4/exercise-1.js
+++ Week-4/exercise-1.js
@@ -10,10 +10,35 @@
     A promise is SETTLED in case it's either fulfilled or rejected.
 */
 
+// Skeleton of actual promise --
+/*
+const promise = new Promise((resolve, reject) => {
+    
+    //  Your code logic goes here and you call resolve(value)
+    //  or reject(value) to resolve or reject the promise
+    
+})
+
+promise.then((value) => {
+    //  logic on success of an operation
+}).catch(error => {
+    //  logic on failure of an operation
+}).finally(() => {
+    // logic to be executed irrespective of failure/success
+});
+*/
+
+const STATE = {
+    PENDING: 'PENDING',
+    FULFILLED: 'FULFILLED',
+    REJECTED: 'REJECTED',
+}
 
 class customPromise {
     constructor(callback) {
+        this.state = STATE.PENDING;
+        this.value = undefined;
         
-
     }
 }
+
